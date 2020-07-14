77b4306757a6232d22d7ad6981a52cd994011906
Raja <raja@kalvad.com>
2020-07-14 19:18:52 +0400

    Added New questions to the polls

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
